359f9737d39e7b7051baf92b4e349d04ffde6019
Markus Felten <Markus.Felten@gmx.de>
2019-02-22 12:10:29 +0100

    fis: use gte & lte

--- src/database.mjs
+++ src/database.mjs
@@ -1,6 +1,8 @@
-const SUPER = "super";
 const CATEGORY_PREFIX = "categories.";
 const VALUE_PREFIX = "values.";
+
+const MASTER = "master";
+const SCHEMA_VERSION = '1'
 
 /**
  * @param {string} name category name
@@ -30,6 +32,26 @@ export class Category {
   }
 
   /**
+   * initialize database
+   * checks/writes master record
+   * @param {levelup} db
+   */
+  async initialize(db) {
+    for await (const data of db.createReadStream({ start: MASTER, end: MASTER })) {
+      const master = JSON.parse(data.value.toString());
+      if(master.schemaVersion !== SCHEMA_VERSION) {
+        throw new Error(`invalid schema version ${master.schemaVersion}`);
+      }
+
+      return master;
+    }
+
+    const master = { schemaVersion: SCHEMA_VERSION };
+    await db.put(MASTER, JSON.stringify(master));
+    return master;
+  }
+
+  /**
    * write the category
    * @param {levelup} db
    */
@@ -42,24 +64,37 @@ export class Category {
    * get all categories
    * @param {levelup} db
    */
-  static async *entries(db, start='A', end='_' ) {
-    for await (const data of db.createReadStream({ start: CATEGORY_PREFIX + start , end: CATEGORY_PREFIX + end })) {
+  static async *entries(db, gte='\u0000', lte='\uFFFF' ) {
+    for await (const data of db.createReadStream({ gte: CATEGORY_PREFIX + gte , lte: CATEGORY_PREFIX + lte })) {
       const name = data.key.toString().substring(CATEGORY_PREFIX.length);
       yield new Category(name, JSON.parse(data.value.toString()));
     }
   }
 
 
-  async insertValue(db, value, time) {
+  /**
+   * write a time/value pair
+   * @param {levelup} db
+   * @param {number} value
+   * @param {string} time
+   */
+  async writeValue(db, value, time) {
     const key = VALUE_PREFIX + this.name + '.' + time;
     return db.put(key, value);
   }
 
-  async * values(db) {
-    const key = VALUE_PREFIX + this.name;
-    for await (const data of db.createReadStream()) {
-
-      yield { value: data.value };
+  /**
+   * get all values of the category
+   * @param {levelup} db
+   * @param {string} gte
+   * @param {string} lte
+   * @return {Iterator<object>}
+   */
+  async * values(db, gte='\u0000', lte='\uFFFF') {
+    const key = VALUE_PREFIX + this.name + '.';
+    for await (const data of db.createReadStream({ gte: key + gte , lte: key + lte })) {
+      const time = data.key.toString().substring(key.length);
+      yield { value: data.value, time };
     }
   }
 }
