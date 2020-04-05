e5601b5a0d3fdaed58445f547e6befd50d9e2253
Markus Felten <Markus.Felten@gmx.de>
2020-04-05 15:55:16 +0200

    feat: meter properties (WIP)

--- src/meter.mjs
+++ src/meter.mjs
@@ -1,3 +1,37 @@
+import { Readable } from "stream";
+import { secondsAsString, definePropertiesFromOptions, optionJSON } from "./util.mjs";
+
+/**
+ * Meter
+ * @param {string} name meter name
+ * @param {Object} options
+ * @param {string} options.description
+ * @param {string} options.unit physical unit like kWh or m3
+ *
+ * @property {string} name category name
+ * @property {string} description
+ * @property {string} unit physical unit
+ */
 export class Meter {
-  constructor(category, identifier) {}
+  static get defaultOptions() {
+    return {
+      /**
+       * the description of the content.
+       * @return {string}
+       */
+      description: undefined,
+
+      /**
+       * physical unit.
+       * @return {string}
+       */
+      unit: undefined
+    };
+  }
+
+  constructor(category, name) {
+    definePropertiesFromOptions(this, options, {
+      name: { value: name }
+    });
+  }
 }
