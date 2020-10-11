f38186e341b8e0a65254b83e2f7c288f832f704e
Markus Felten <Markus.Felten@gmx.de>
2020-10-11 19:11:07 +0200

    test: wip

--- tests/note-test.mjs
+++ tests/note-test.mjs
@@ -1,4 +1,7 @@
 import test from "ava";
+import tmp from "tmp";
+import levelup from "levelup";
+import leveldown from "leveldown";
 import { secondsAsString } from "../src/util.mjs";
 import { Category, Note } from "konsum-db";
 
@@ -9,4 +12,29 @@ test("create Note", async t => {
 
   t.is(n1.key, "notes.CAT-1." + secondsAsString(time));
   t.is(n1.description, "some text");
+});
+
+test.skip("Note write / read", async t => {
+  const db = await levelup(leveldown(tmp.tmpNameSync()));
+
+  const c = new Category("CAT-1", { unit: "kWh", fractionalDigits: 3 });
+  await c.write(db);
+
+  const time = Date.now();
+
+  for (let i = 0; i < 5; i++) {
+    const n = new Note(time + i, c, { description: `description ${i}` });
+    await n.write(db);
+  }
+
+  const ms = [];
+
+  for await (const m of Note.entries(db)) {
+    ms.push(m);
+  }
+
+  t.true(ms.length >= 5);
+  t.is(ms[0].description, "description 0");
+
+  db.close();
 });
