e72bd3b99a19c2bcafeb00931e01f3419cf5079d
Markus Felten <Markus.Felten@gmx.de>
2024-05-02 21:37:47 +0100

    test: make use of testWriteReadUpdateDeleteCategories()

--- tests/category-ava-node.mjs
+++ tests/category-ava-node.mjs
@@ -2,7 +2,7 @@ import test from "ava";
 import tmp from "tmp";
 import {
   testCategoryConstructor,
-  testWriteReadDeleteCategoriest
+  testWriteReadUpdateDeleteCategories
 } from "@konsumation/db-test";
 import { LevelMaster, LevelCategory } from "@konsumation/db-level";
 import { CATEGORY_PREFIX } from "../src/consts.mjs";
@@ -13,8 +13,8 @@ test("Category key", t =>
   t.is(new LevelCategory({ name: "name1" }).key, "categories.name1"));
 test("Category constructor", t => testCategoryConstructor(t, LevelCategory));
 
-test("Category write / read / delete", async t =>
-  testWriteReadDeleteCategoriest(
+test("Category write / read / update / delete", async t =>
+  testWriteReadUpdateDeleteCategories(
     t,
     await LevelMaster.initialize(tmp.tmpNameSync())
   ));
